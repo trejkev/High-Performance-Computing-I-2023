d40158e76def4ef9af5dca4404b0e82fa128b45c
Kevin Trejos Vargas <trejkev@gmail.com>
2023-05-23 00:46:04 -0600

    Modified to receive from CLI the replicas to run the main part of the code

--- Homework_3/src/MatricesVectorialMultiplier.c
+++ Homework_3/src/MatricesVectorialMultiplier.c
@@ -1,9 +1,18 @@
 #include <omp.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>  // To deal with delta time measurements
+#include <errno.h>
 
 // Code to Perform C = AxB
 int main(int argc, char** argv) {
+    size_t replicas;
+    if (argc == 2) {
+        if (sscanf(argv[1], "%zu", &replicas) != 1 || errno) {
+            fprintf(stderr, "Invalid number of replicas.\n");
+            return EXIT_FAILURE;
+        }
+    }
     size_t Ndim = 3;  // Number of rows in A
     size_t Mdim = 4;  // Number of columns in B
     size_t Pdim = 5;  // Number of columns in matrix A = rows in matrix B
@@ -29,22 +38,31 @@ int main(int argc, char** argv) {
 
     size_t i, j, k;
 
-    // Ndim for rows in A
-    // Mdim for cols in B
-    // Pdim for columns in A = rows in B
-    #pragma omp parallel for private(i, j, k) shared(A, B, C)
-    for (i = 0; i < Ndim ; i++) {
-        for (j = 0; j < Mdim ; j++) {
-            double tmp = 0;
-                for (k = 0; k < Pdim ; k++) {
-                    /* C (i , j ) = sum ( over k ) A (i , k ) * B (k , j ) */
-                    tmp += *(A+(i*Pdim+k)) * *(B+(k*Mdim+j)); // Same as tmp += A[i*Pdim+k] * B[k*Mdim+j];
-                }
-            C[i*Mdim+j] = tmp;
-        }
-    }
+    for (size_t replica = 0; replica < replicas; ++replica){
 
-    // Print the result matrix
+        float start_time = clock();
+
+        // Ndim for rows in A
+        // Mdim for cols in B
+        // Pdim for columns in A = rows in B
+        #pragma omp parallel for private(i, j, k) shared(A, B, C)
+        for (i = 0; i < Ndim ; i++) {
+            for (j = 0; j < Mdim ; j++) {
+                double tmp = 0;
+                    for (k = 0; k < Pdim ; k++) {
+                        /* C (i , j ) = sum ( over k ) A (i , k ) * B (k , j ) */
+                        tmp += *(A+(i*Pdim+k)) * *(B+(k*Mdim+j)); // Same as tmp += A[i*Pdim+k] * B[k*Mdim+j];
+                    }
+                C[i*Mdim+j] = tmp;
+            }
+        }
+
+        float finish_time = clock();
+        float fElapsedTime = (float)(finish_time - start_time)/CLOCKS_PER_SEC;
+
+        // Print the result matrix
+        printf("%zu - Elapsed time: %.9lf\n", replica, fElapsedTime);
+    }
     printf("Result:\n");
     for (size_t i = 0; i < Ndim; i++) {
         for (size_t j = 0; j < Mdim; j++) {
