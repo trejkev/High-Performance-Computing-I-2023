6f49ac473ad38e528992d1045b30f51463e10d98
Kevin Trejos Vargas <trejkev@gmail.com>
2023-06-08 20:09:43 -0600

    Dealing with multiple signal types, and storing them separately

--- Project_Fast_Fourier_Transform/src/signal_generator.c
+++ Project_Fast_Fourier_Transform/src/signal_generator.c
@@ -1,31 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-#ifndef PI
-#define PI 3.1415926535897932384626433832795028841971693993751058209749445923
-#endif
 
 int main(int argc, char *argv[]) {
     FILE *fptr;
-    char* sFileName = (char*)malloc(200*sizeof(char));
+    char* sSignalDesc = (char*)malloc(200*sizeof(char));
+    char* sSignalPath = (char*)malloc(400*sizeof(char));
     size_t iSamples;
     if (argc == 3) {
-        sscanf(argv[1], "%s", sFileName);
+        sscanf(argv[1], "%s", sSignalDesc);
         sscanf(argv[2], "%zu", &iSamples);
     }
-    if((fptr = fopen("./test/signalA.txt", "w")) != NULL) {
+
+    strcpy(sSignalPath, "./test/");
+    strcat(sSignalPath, sSignalDesc);
+    strcat(sSignalPath, ".txt");
+    printf("Signal path is %s\n", sSignalPath);
+
+    if((fptr = fopen(sSignalPath, "w")) != NULL) {
         fprintf(fptr, "%zu\n", iSamples);
         fprintf(fptr, "%zu\n", iSamples);
-        for (int iCounter = 0; iCounter < iSamples; iCounter++) {
-            float fTime = (float)iCounter/iSamples;
-            float fSample = sin(2*PI*50*fTime) +
-                sin(2*PI*20*fTime);
+        float fTimeStep = 1.0/iSamples;
+        for (size_t iCounter = 0; iCounter < iSamples; iCounter++) {
+            float t = iCounter*fTimeStep;
+            float fSample;
+            if (strcmp(sSignalDesc, "sin_50Hz") == 0) {
+                fSample = sin(2.0*M_PI*50.0*t);
+            } else if (strcmp(sSignalDesc, "sin_50Hz_PLUS_sin_450Hz") == 0) {
+                fSample = sin(2.0*M_PI*50.0*t) + sin(2.0*M_PI*450.0*t);
+            } else if (strcmp(sSignalDesc, "4_PLUS_sin_50Hz_PLUS_sin_450Hz") == 0) {
+                fSample = 4 + sin(2.0*M_PI*50.0*t) + sin(2.0*M_PI*450.0*t);
+            } else if (strcmp(sSignalDesc, "sin_1Hz") == 0) {
+                fSample = sin(2.0*M_PI*1.0*t);
+            } else if (strcmp(sSignalDesc, "cos_1Hz") == 0) {
+                fSample = cos(2.0*M_PI*1.0*t);
+            } else {
+                printf("%s is not a valid signal.\n", sSignalDesc);
+                return 1;
+            }
             fprintf(fptr, "%f\n", fSample);
         }
     } else {
-        printf("Failed to open signalA.txt\n");
+        printf("Failed to open %s\n", sSignalPath);
     }
-    free(sFileName);
+    free(sSignalPath);
+    free(sSignalDesc);
     return 0;
 }
