95137c5aa2d4dd4c05495fea6d1c6c057c08c24f
Kevin Trejos Vargas <trejkev@gmail.com>
2023-06-24 00:08:20 -0600

    Corrected memory usage from device and host

--- Practice/Final_Exam_Practice/Ejercicio_3/addElements.cu
+++ Practice/Final_Exam_Practice/Ejercicio_3/addElements.cu
@@ -8,10 +8,16 @@
 __global__ void add(int n, float *x, float *y) {
   int iSliceSize = n/(blockDim.x*gridDim.x);
   int iInit      = threadIdx.x*iSliceSize + blockIdx.x*iSliceSize*blockDim.x;
-  printf("Thread  %d, block %d, silice size %d, init %d, end %d\n",
-    threadIdx.x, blockIdx.x, iSliceSize, iInit, iInit + iSliceSize - 1);
-  for (int iIndex = iInit; iIndex < iInit + iSliceSize; iInit++) {
-    y[iIndex] = x[iIndex] + y[iIndex];
+  int iEnd       = iInit + iSliceSize;
+  
+  // Adjust iEnd if this is the last block and thread
+  if (blockIdx.x == gridDim.x - 1 && threadIdx.x == blockDim.x - 1) {
+    iEnd = n;
+  }
+  
+  // add the input data
+  for (int iIndex = iInit; iIndex < iEnd; ++iIndex) {
+    y[iIndex] += x[iIndex];
   }
 }
 
@@ -19,40 +25,47 @@ int main(void) {
   int N = 1<<20; // 1M elements
   printf("N is %d\n", N);
 
-  float *x = new float[N];
-  float *y = new float[N];
-  //float *x;
-  //float *y;
-  //cudaMallocManaged(&x, N*sizeof(float));
-  //cudaMallocManaged(&y, N*sizeof(float));
+  float *fXHost = (float*)calloc(N, sizeof(float));
+  float *fYHost = (float*)calloc(N, sizeof(float));
 
   // initialize x and y arrays on the host
   for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
+    fXHost[i] = 1.0f;
+    fYHost[i] = 2.0f;
   }
   
+  // Allocate memory for device
+  float *fXDevice;
+  float *fYDevice;
+  cudaMalloc((void**)&fXDevice, N*sizeof(float));
+  cudaMalloc((void**)&fYDevice, N*sizeof(float));
+  cudaMemcpy(fXDevice, fXHost, N*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(fYDevice, fYHost, N*sizeof(float), cudaMemcpyHostToDevice);
+
   int iBlocks  = 10000;
   int iThreads = 100;
-  printf("Will run GPU concurrency with %d blocks and %d threads per block\n",
-    iBlocks, iThreads);
+  printf("Will run GPU concurrency with %d blocks and %d threads per block\n", iBlocks, iThreads);
   printf("Just about to execute GPU\n");
   // Run kernel on 1M elements on the CPU
-  add<<<iBlocks, iThreads>>>(N, x, y);
+  add<<<iBlocks, iThreads>>>(N, fXDevice, fYDevice);
   cudaDeviceSynchronize();
   printf("Out of GPU region\n");
+
+  cudaMemcpy(fYHost, fYDevice, N*sizeof(float), cudaMemcpyDeviceToHost);
 
   // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
   for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
+    maxError = fmax(maxError, fabs(fYHost[i]-3.0f));
   std::cout << "Max error: " << maxError << std::endl;
 
   // Free memory
-  delete [] x;
-  delete [] y;
-  //cudaFree(x);
-  //cudaFree(y);
+  //delete [] x;
+  //delete [] y;
+  cudaFree(fXDevice);
+  cudaFree(fYDevice);
+  free(fXHost);
+  free(fYHost);
 
   return 0;
 }
